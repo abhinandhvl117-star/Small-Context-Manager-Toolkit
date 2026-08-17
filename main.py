0f9c318bf87d7dc09a6b1eeb745f9d68794e5074
Abhinandh vijayan <abhinandhvl117@gmail.com>
2026-08-17 20:20:55 +0530

    finished everything

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import time
 from contextlib import contextmanager
 
-# class Timer:
+class Timer:
  
-#     def __enter__(self):
-#         self.start = time.time()
+    def __enter__(self):
+        self.start = time.time()
     
-#     def __exit__(self, exc_type, exc, tb):
-#         self.end = time.time()
-#         print(round(self.end - self.start, 2)) 
+    def __exit__(self, exc_type, exc, tb):
+        self.end = time.time()
+        print(round(self.end - self.start, 2)) 
         
-# try:
-#     with Timer():
-#         time.sleep(1)
-#         raise ValueError("Something went wrong")
-# except ValueError:
-#     print("Exception caught")
+try:
+    with Timer():
+        time.sleep(1)
+        raise ValueError("Something went wrong")
+except ValueError:
+    print("Exception caught")
 
-# @contextmanager
-# def timer():
-#     try:
-#         start = time.time()
-#         yield
-#     finally:
-#         end = time.time()
-#         print(round(end - start, 2))
+@contextmanager
+def timer():
+    try:
+        start = time.time()
+        yield
+    finally:
+        end = time.time()
+        print(round(end - start, 2))
         
-# try:
-#     with timer():
-#         time.sleep(1)
-#         raise ValueError("Oops")
-# except ValueError:
-#     print("Caught")
+try:
+    with timer():
+        time.sleep(1)
+        raise ValueError("Oops")
+except ValueError:
+    print("Caught")
     
     
 @contextmanager
